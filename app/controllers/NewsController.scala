2bddc9b2d94c0142f4f69dc899a2f953aee4b5ea
Suo Qin <kpsesuo@gmail.com>
2013-12-08 20:31:32 +0800

    backend save the change of news

--- app/controllers/NewsController.scala
+++ app/controllers/NewsController.scala
@@ -3,6 +3,8 @@ package controllers
 import play.api.mvc._
 import play.api.libs.json.Json
 import models.News
+import play.api.data.Form
+import play.api.data.Forms._
 
 object NewsController extends Controller {
   implicit val writes = Json.writes[News]
@@ -19,4 +21,20 @@ object NewsController extends Controller {
     }.getOrElse(NotFound)
   }
 
+  val newsForm = Form(
+    tuple(
+      "id" -> longNumber,
+      "k_id" -> longNumber,
+      "content" -> text
+    )
+  )
+
+  def update(kg: String, newsId: Long) = Action {
+    implicit request =>
+      newsForm.bindFromRequest.value map {
+        news =>
+          val update1 = News.update(news)
+          Ok(Json.toJson(update1)).as("application/json")
+      } getOrElse BadRequest
+  }
 }
--- app/models/News.scala
+++ app/models/News.scala
@@ -11,6 +11,22 @@ case class News(id: Long, k_id: Long, title: String, content: String, issueDate:
 
 
 object News {
+  def update(form: (Long, Long, String)) = DB.withConnection {
+    implicit c =>
+      SQL("update news set content={content} where id={id}")
+        .on('content -> form._3)
+        .on('id -> form._1).executeUpdate()
+      findById(form._1)
+  }
+
+  def findById(id: Long) = DB.withConnection {
+    implicit c =>
+      SQL("select * from news where id={id}")
+        .on('id -> id)
+        .as(simple.singleOpt)
+  }
+
+
   def findById(kg: String)(id: Long) = DB.withConnection {
     implicit c =>
       SQL("select a.* from news a, kindergarten k where k.id = a.k_id and k.name={kg} and a.id={id}")
