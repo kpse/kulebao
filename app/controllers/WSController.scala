7203b96c8ae38b36adfd546cfedd1132dee7b2f3
Suo Qin <kpsesuo@gmail.com>
2013-12-22 22:46:40 +0800

    add group message backend

--- app/controllers/WSController.scala
+++ app/controllers/WSController.scala
@@ -3,8 +3,11 @@ package controllers
 import play.api.mvc._
 import play.api.libs.ws._
 import play.api.libs.json.{JsValue, Json}
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 import ExecutionContext.Implicits.global
+import models.News
+import scala.Some
+import play.api.Logger
 
 object WSController extends Controller {
 
@@ -20,8 +23,8 @@ object WSController extends Controller {
   def postData(pushId: Option[String]): JsValue = {
     Json.obj(
       "timestamp" -> System.currentTimeMillis,
-      "notice_type" -> "1",
-      "child_id" -> "3",
+      "notice_type" -> 1,
+      "child_id" -> 3,
       "pushid" -> Json.toJson(pushId.getOrElse("925387477040814447")),
       "record_url" -> "http://a.hiphotos.baidu.com/image/w%3D2048/sign=0ae610304890f60304b09b470d2ab21b/10dfa9ec8a136327e2b45e32938fa0ec08fac752.jpg"
     )
@@ -39,6 +42,40 @@ object WSController extends Controller {
         response =>
           Ok(response.json)
       }
+  }
+
+  def broadcastMessage(news: News): JsValue = {
+    Json.obj(
+      "timestamp" -> System.currentTimeMillis,
+      "notice_type" -> 2,
+      "push_tags" -> news.school_id,
+      "notice_title" -> news.title,
+      "notice_body" -> news.content,
+      "publisher" -> "华德福幼儿园"
+    )
+  }
+
+  def sendBroadcastMessage = Action.async(parse.json) {
+    request =>
+      val body: JsValue = request.body
+      val schoolId: Long = (body \ "school_id").as[Long]
+      val newsId: Long = (body \ "news_id").as[Long]
+      val news = News.findById(schoolId, newsId)
+      Logger.info("sending message of school %d, news %d".format(schoolId, newsId))
+      if (!news.isEmpty) {
+        val url = "http://djcwebtest.duapp.com/nmn.do"
+        WS.url(url).post(broadcastMessage(news.get)).map {
+          response =>
+            Ok(response.json)
+        }
+      } else {
+        Future {
+          BadRequest("No such message.")
+        }
+
+      }
+
+
   }
 
 }
