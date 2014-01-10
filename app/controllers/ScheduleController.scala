b44a4cfe960e165f5a3516468c1bf0bb54256599
Suo Qin <kpsesuo@gmail.com>
2014-01-11 01:14:40 +0800

    real schedule show impl

--- app/controllers/ScheduleController.scala
+++ app/controllers/ScheduleController.scala
@@ -5,24 +5,19 @@ import play.api.libs.json.Json
 import models._
 import models.DaySchedule
 import models.SchedulePreviewResponse
-import models.ScheduleResponse
 import models.WeekSchedule
 
 object ScheduleController extends Controller {
   implicit val write1 = Json.writes[SchedulePreviewResponse]
   implicit val write2 = Json.writes[DaySchedule]
   implicit val write3 = Json.writes[WeekSchedule]
-  implicit val write4 = Json.writes[ScheduleResponse]
-
-  //[{"timestamp":1387468726467,"schedule_id":"123","class_id":"321","error_code":0,"school_id":"12344"}]
+  implicit val write4 = Json.writes[ScheduleDetail]
 
   def preview(kg: Long, classId: Long) = Action {
-    val list = List(new SchedulePreviewResponse(0, kg, classId, 123, System.currentTimeMillis))
     Ok(Json.toJson(Schedule.preview(kg, classId)))
   }
 
   def show(kg: Long, classId: Long, scheduleId: Long) = Action {
-    Ok(Json.toJson(new ScheduleResponse(0, kg, classId, scheduleId, System.currentTimeMillis, new WeekSchedule(new DaySchedule("手工1", "木工"),
-      new DaySchedule("手工2", "木工木工木工木工木工木工"), new DaySchedule("手工3", "水管工"), new DaySchedule("手工4", "电工"), new DaySchedule("手工5", "钳工")))))
+    Ok(Json.toJson(Schedule.show(kg, classId, scheduleId)))
   }
 }
--- app/models/Schedule.scala
+++ app/models/Schedule.scala
@@ -12,10 +12,19 @@ case class DaySchedule(am: String, pm: String)
 
 case class WeekSchedule(mon: DaySchedule, tue: DaySchedule, wed: DaySchedule, thu: DaySchedule, fri: DaySchedule)
 
-case class ScheduleResponse(error_code: Int, school_id: Long, class_id: Long, schedule_id: Long, timestamp: Long, week: WeekSchedule)
+case class ScheduleDetail(error_code: Int, school_id: Long, class_id: Long, schedule_id: Long, timestamp: Long, week: WeekSchedule)
 
 
 object Schedule {
+  def show(kg: Long, classId: Long, scheduleId: Long) = DB.withConnection {
+    implicit c =>
+      SQL("select * from scheduleinfo where status=1 and school_id={school_id} and class_id={class_id} and schedule_id={schedule_id}")
+        .on('school_id -> kg.toString,
+          'class_id -> classId,
+          'schedule_id -> scheduleId
+        ).as(detail singleOpt)
+  }
+
   val previewSimple = {
     get[String]("school_id") ~
       get[Int]("class_id") ~
@@ -32,5 +41,33 @@ object Schedule {
         .on('school_id -> kg.toString,
           'class_id -> classId
         ).as(previewSimple *)
+  }
+
+  val detail = {
+    get[String]("school_id") ~
+      get[Int]("class_id") ~
+      get[Int]("schedule_id") ~
+      get[Long]("timestamp") ~
+      get[String]("mon_am") ~
+      get[String]("tue_am") ~
+      get[String]("wed_am") ~
+      get[String]("thu_am") ~
+      get[String]("fri_am") ~
+      get[String]("mon_pm") ~
+      get[String]("tue_pm") ~
+      get[String]("wed_pm") ~
+      get[String]("thu_pm") ~
+      get[String]("fri_pm") map {
+      case school_id ~ class_id ~ schedule_id ~ timestamp ~
+        mon_am ~ tue_am ~ wed_am ~ thu_am ~ fri_am ~
+        mon_pm ~ tue_pm ~ wed_pm ~ thu_pm ~ fri_pm =>
+        val schedule = new WeekSchedule(
+          new DaySchedule(mon_am, mon_pm),
+          new DaySchedule(tue_am, tue_pm),
+          new DaySchedule(wed_am, wed_pm),
+          new DaySchedule(thu_am, thu_pm),
+          new DaySchedule(fri_am, fri_pm))
+        ScheduleDetail(0, school_id.toLong, class_id, schedule_id, timestamp, schedule)
+    }
   }
 }
