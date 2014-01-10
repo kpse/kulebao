95a41e4e90a05449826c9fea8177b5049520a0f8
Suo Qin <kpsesuo@gmail.com>
2014-01-11 01:22:44 +0800

    add update method of schedule

--- app/controllers/ScheduleController.scala
+++ app/controllers/ScheduleController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, Json}
 import models._
 import models.DaySchedule
 import models.SchedulePreviewResponse
@@ -12,6 +12,9 @@ object ScheduleController extends Controller {
   implicit val write2 = Json.writes[DaySchedule]
   implicit val write3 = Json.writes[WeekSchedule]
   implicit val write4 = Json.writes[ScheduleDetail]
+  implicit val read1 = Json.reads[DaySchedule]
+  implicit val read2 = Json.reads[WeekSchedule]
+  implicit val read3 = Json.reads[ScheduleDetail]
 
   def preview(kg: Long, classId: Long) = Action {
     Ok(Json.toJson(Schedule.preview(kg, classId)))
@@ -20,4 +23,15 @@ object ScheduleController extends Controller {
   def show(kg: Long, classId: Long, scheduleId: Long) = Action {
     Ok(Json.toJson(Schedule.show(kg, classId, scheduleId)))
   }
+
+  def update(kg: Long, classId: Long, scheduleId: Long) = Action(parse.json) {
+    implicit request =>
+      request.body.validate[ScheduleDetail].map {
+        case (detail) =>
+          Ok(Json.toJson(Schedule.insertNew(detail)))
+      }.recoverTotal {
+        e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+      }
+  }
+
 }
--- app/models/Schedule.scala
+++ app/models/Schedule.scala
@@ -16,6 +16,45 @@ case class ScheduleDetail(error_code: Int, school_id: Long, class_id: Long, sche
 
 
 object Schedule {
+  def insertNew(schedule: ScheduleDetail)  = DB.withConnection {
+    implicit c =>
+      SQL("update scheduleinfo set status=0 where school_id={school_id} and class_id={class_id} and schedule_id={schedule_id}")
+        .on('school_id -> schedule.school_id.toString,
+          'class_id -> schedule.class_id,
+          'schedule_id -> schedule.schedule_id
+        ).executeUpdate
+
+      val newId : Option[Long] = SQL("insert into scheduleinfo set school_id={school_id}, " +
+        "schedule_id={schedule_id}, class_id={class_id}, timestamp={timestamp}, " +
+        "mon_pm={mon_pm}, mon_am={mon_am}, " +
+        "tue_pm={tue_pm}, tue_am={tue_am}, " +
+        "wed_pm={wed_pm}, wed_am={wed_am}, " +
+        "thu_pm={thu_pm}, thu_am={thu_am}, " +
+        "fri_pm={fri_pm}, fri_am={fri_am}")
+        .on('school_id -> schedule.school_id.toString,
+          'schedule_id -> (schedule.schedule_id + 1),
+          'class_id -> schedule.class_id,
+          'timestamp -> System.currentTimeMillis,
+          'mon_am -> schedule.week.mon.am,
+          'tue_am -> schedule.week.tue.am,
+          'wed_am -> schedule.week.wed.am,
+          'thu_am -> schedule.week.thu.am,
+          'fri_am -> schedule.week.fri.am,
+          'mon_pm -> schedule.week.mon.pm,
+          'tue_pm -> schedule.week.tue.pm,
+          'wed_pm -> schedule.week.wed.pm,
+          'thu_pm -> schedule.week.thu.pm,
+          'fri_pm -> schedule.week.fri.pm
+        ).executeInsert()
+      findById(newId.get)
+  }
+  def findById(uid: Long) = DB.withConnection {
+    implicit c =>
+      SQL("select * from scheduleinfo where status=1 and uid={uid}")
+        .on('uid -> uid
+        ).as(detail singleOpt)
+  }
+
   def show(kg: Long, classId: Long, scheduleId: Long) = DB.withConnection {
     implicit c =>
       SQL("select * from scheduleinfo where status=1 and school_id={school_id} and class_id={class_id} and schedule_id={schedule_id}")
