df9247600148d10c3edc73e3e6344f953181d6ae
Suo Qin <kpsesuo@gmail.com>
2014-01-23 23:34:01 +0800

    refactor to remove the duplicated code between tag and single push

--- app/controllers/PushController.scala
+++ app/controllers/PushController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import com.baidu.yun.channel.auth.ChannelKeyPair
 import com.baidu.yun.channel.client.BaiduChannelClient
-import com.baidu.yun.channel.model.{PushTagMessageResponse, PushTagMessageRequest, PushUnicastMessageRequest, PushUnicastMessageResponse}
+import com.baidu.yun.channel.model._
 import com.baidu.yun.core.log.{YunLogEvent, YunLogHandler}
 import com.baidu.yun.channel.exception.{ChannelServerException, ChannelClientException}
 import play.Logger
@@ -10,10 +10,12 @@ import play.api.mvc._
 import play.api.libs.json.{JsError, Json}
 import models.json_models.CheckingMessage
 import scala.Predef._
-import models.{News, JsonResponse, SuccessResponse, ErrorResponse}
-import models.json_models.CheckNotification
-import models.json_models.CheckInfo
+import models._
 import play.api.Play
+import models.json_models.CheckInfo
+import scala.Some
+import models.json_models.CheckNotification
+import models.JsonResponse
 
 object PushController extends Controller {
   implicit val write = Json.writes[CheckNotification]
@@ -21,38 +23,28 @@ object PushController extends Controller {
   implicit val read = Json.reads[CheckInfo]
 
   def test = Action {
-    Ok(Json.toJson(pushMessage(new CheckNotification(System.currentTimeMillis, 1, "1_93740362_374", "925387477040814447", ""))))
+    Ok(Json.toJson(runWithLog(new CheckNotification(System.currentTimeMillis, 1, "1_93740362_374", "925387477040814447", ""), triggerSinglePush)))
   }
+
+  type Trigger[A] = (A) => ChannelResponse
 
   def testGroup(newsId: Option[Long]) = Action {
     News.findById(93740362L, newsId.getOrElse(2L)) match {
       case Some(n: News) =>
-        Ok(Json.toJson(tagPushMsg(n)))
+        Ok(Json.toJson(runWithLog(n, triggerTagPush)))
       case None =>
         Ok(Json.toJson(new ErrorResponse("No such news")))
     }
 
   }
 
-
-  def pushMessage(check: CheckNotification): JsonResponse = {
-    val channelClient = getClient
-
+  def runWithLog[A](msg: A, trigger: Trigger[A]) = {
     try {
-      val request: PushUnicastMessageRequest = new PushUnicastMessageRequest
-      request.setDeviceType(new Integer(3))
-      request.setUserId(check.pushid)
-      request.setMessageType(new Integer(0))
-      Logger.info(Json.toJson(check).toString)
-      request.setMessage(Json.toJson(check).toString)
-      request.setMsgKey(check.timestamp.toString)
-      val response: PushUnicastMessageResponse = channelClient.pushUnicastMessage(request)
-      Logger.info("push amount : " + response.getSuccessAmount)
+      trigger(msg)
       new SuccessResponse
     }
     catch {
       case e: ChannelClientException => {
-        Logger.info(e.printStackTrace.toString)
         new ErrorResponse(e.printStackTrace.toString)
       }
       case e: ChannelServerException => {
@@ -63,6 +55,34 @@ object PushController extends Controller {
     }
   }
 
+
+  def triggerSinglePush(check: CheckNotification) = {
+    val channelClient = getClient
+    val request: PushUnicastMessageRequest = new PushUnicastMessageRequest
+    request.setDeviceType(new Integer(3))
+    request.setUserId(check.pushid)
+    request.setMessageType(new Integer(0))
+    Logger.info(Json.toJson(check).toString)
+    request.setMessage(Json.toJson(check).toString)
+    request.setMsgKey(check.timestamp.toString)
+    val response: PushUnicastMessageResponse = channelClient.pushUnicastMessage(request)
+    Logger.info("push amount : " + response.getSuccessAmount)
+    response
+  }
+
+  def triggerTagPush(news: News) = {
+    val channelClient = getClient
+    val request: PushTagMessageRequest = new PushTagMessageRequest
+    request.setDeviceType(3)
+    request.setTagName(news.school_id.toString)
+    request.setMessageType(0)
+    request.setMessage("{\"notice_title\":\"" + news.title + "\",\"notice_body\":\"" + news.content + "\", \"notice_type\":2, \"timestamp\": " + System.currentTimeMillis + ", \"publisher\":\"老师\"}")
+    request.setMsgKey(System.currentTimeMillis.toString)
+    request.setMessageExpires(7 * 86400)
+    val response: PushTagMessageResponse = channelClient.pushTagMessage(request)
+    Logger.info("push amount : " + response.getSuccessAmount)
+    response
+  }
 
   def getClient = {
 
@@ -81,36 +101,9 @@ object PushController extends Controller {
     channelClient
   }
 
-
-  def tagPushMsg(news: News) = {
-    val channelClient: BaiduChannelClient = getClient
-    try {
-      val request: PushTagMessageRequest = new PushTagMessageRequest
-      request.setDeviceType(3)
-      request.setTagName(news.school_id.toString)
-      request.setMessageType(0)
-      request.setMessage("{\"notice_title\":\"" + news.title + "\",\"notice_body\":\"" + news.content + "\", \"notice_type\":2, \"timestamp\": " + System.currentTimeMillis + ", \"publisher\":\"老师\"}")
-      request.setMsgKey(System.currentTimeMillis.toString)
-      request.setMessageExpires(7 * 86400)
-      val response: PushTagMessageResponse = channelClient.pushTagMessage(request)
-      Logger.info("push amount : " + response.getSuccessAmount)
-      new SuccessResponse
-    }
-    catch {
-      case e: ChannelClientException => {
-        new ErrorResponse(e.printStackTrace.toString)
-      }
-      case e: ChannelServerException => {
-        val error = "request_id: %d, error_code: %d, error_message: %s".format(e.getRequestId, e.getErrorCode, e.getErrorMsg)
-        Logger.info(error)
-        new ErrorResponse(error)
-      }
-    }
-  }
-
   def createSwipeMessage(check: Option[CheckNotification]) = {
     check map {
-      c => pushMessage(c)
+      c => runWithLog(c, triggerSinglePush)
     }
   }
 
@@ -119,9 +112,7 @@ object PushController extends Controller {
       Logger.info("checking : " + request.body)
       request.body.validate[CheckInfo].map {
         case (check) =>
-          val notification = CheckingMessage.convert(check)
-
-          Ok(Json.toJson(createSwipeMessage(notification)))
+          Ok(Json.toJson(createSwipeMessage(CheckingMessage.convert(check))))
       }.recoverTotal {
         e => BadRequest("Detected error:" + JsError.toFlatJson(e))
       }
