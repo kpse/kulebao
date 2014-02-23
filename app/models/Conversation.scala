62bda85d56da6fa2c76d546d221afd94dbe9d121
Suo Qin <kpsesuo@gmail.com>
2014-02-23 22:31:58 +0800

    fix from/to/most, remove sort

--- app/models/Conversation.scala
+++ app/models/Conversation.scala
@@ -5,7 +5,7 @@ import play.api.db.DB
 import anorm._
 import anorm.SqlParser._
 
-case class Conversation(phone: String, timestamp: Long, id: Option[Long], content: String, image: Option[String], sender: String)
+case class Conversation(phone: String, timestamp: Long, id: Option[Long], content: String, image: Option[String], sender: Option[String])
 
 object Conversation {
   val simple = {
@@ -13,10 +13,10 @@ object Conversation {
       get[Long]("timestamp") ~
       get[Long]("uid") ~
       get[String]("content") ~
-      get[String]("image") ~
-      get[String]("sender") map {
+      get[Option[String]]("image") ~
+      get[Option[String]]("sender") map {
       case phone ~ t ~ id ~ content ~ image ~ sender =>
-        Conversation(phone, t, Some(id), content, Some(image), sender)
+        Conversation(phone, t, Some(id), content, image, sender)
     }
   }
 
@@ -35,20 +35,40 @@ object Conversation {
       Conversation(conversation.phone, time, id, conversation.content, conversation.image, conversation.sender)
   }
 
-  def parseSort(sort: Option[String]) : String = {
-    sort match {
-      case Some(s) if s.matches("asc|desc") => "order by uid " + s
-      case _ => "order by uid asc"
+
+  def generateFrom(from: Option[Long]) = {
+    from map {
+      f =>
+        " and uid > {from}"
     }
   }
 
-  def index(kg: Long, phone: String, most: Option[Int], sort: Option[String]) = DB.withConnection {
+  def generateTo(to: Option[Long]) = {
+    to map {
+      t =>
+        " and uid < {to}"
+    }
+  }
+
+  def generateSort(from: Option[Long], to: Option[Long]) = {
+    (from, to) match {
+      case (Some(f), None) =>
+        "asc"
+      case _ =>
+        "desc"
+    }
+  }
+
+  def index(kg: Long, phone: String, from: Option[Long], to: Option[Long]) = DB.withConnection {
     implicit c =>
-      SQL("select * from conversation where school_id={kg} and phone={phone} " + parseSort(sort) + " limit {most} ")
+      SQL("select * from conversation where school_id={kg} and phone={phone} " +
+        generateFrom(from).getOrElse("") + generateTo(to).getOrElse("")
+        + " order by uid " + generateSort(from, to))
         .on(
           'kg -> kg.toString,
           'phone -> phone,
-          'most -> most.getOrElse(25)
+          'from -> from,
+          'to -> to
         ).as(simple *)
   }
 
